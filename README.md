27a97eff0369c97148b31ede7fa69a12ec189a75
qlhan <qlhan@incarcloud.com>
2020-11-13 17:02:10 +0800

    0.1.0-SNAPSHOT swagger api doc

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 [![license](https://img.shields.io/badge/license-MIT-green.svg?style=flat&logo=github)](https://www.mit-license.org)
 [![java](https://img.shields.io/badge/java-1.8-brightgreen.svg?style=flat&logo=java)](https://www.oracle.com/java/technologies/javase-downloads.html)
+[![spring](https://img.shields.io/badge/java-2.3.2.RELEASE-brightgreen.svg?style=flat&logo=spring)](https://docs.spring.io/spring-boot/docs/2.3.x-SNAPSHOT/reference/htmlsingle)
 [![gradle](https://img.shields.io/badge/gradle-6.7-brightgreen.svg?style=flat&logo=gradle)](https://docs.gradle.org/6.7/userguide/installation.html)
-[![build](https://github.com/aaric/grpc-achieve/workflows/build/badge.svg)](https://github.com/aaric/grpc-achieve/actions)
+[![build](https://github.com/aaric/grpc-achieve/workflows/build/badge.svg)](https://github.com/aaric/promql-achieve/actions)
 [![release](https://img.shields.io/badge/release-0.1.0-blue.svg)](https://github.com/aaric/promql-achieve/releases)
 
 > Simple chart sample with PromQL.
---
+++ src/main/java/com/example/pa/config/Swagger2Config.java
@@ -0,0 +1,43 @@
+package com.example.pa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger2配置
+ *
+ * @author Aaric, created on 2020-11-13T16:08.
+ * @version 0.1.0-SNAPSHOT
+ */
+@EnableSwagger2
+@Configuration
+public class Swagger2Config {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("在线API文档")
+                .description("测试Prometheus API监控和图表功能。")
+                .termsOfServiceUrl("http://localhost:8080")
+                .contact(new Contact("Aaric", "", "vipaaric@gmail.com"))
+                .version("1.1.0")
+                .build();
+    }
+}
